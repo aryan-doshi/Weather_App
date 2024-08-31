b66f330ad1799fa9ae7dfd286431fa28ddce8c7a
Aryan Doshi <108112007+aryan-doshi@users.noreply.github.com>
2024-08-31 14:29:21 +0530

    Create SearchBox.jsx

---
+++ src/SearchBox.jsx
@@ -0,0 +1,74 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+
+export default function SearchBox({ updateInfo }) {
+  let [city, setCity] = useState("");
+  let [error, setError] = useState(false);
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "4fc5b0b1b08a50145fcaec3e63179ba3";
+
+  let getWeatherInfo = async () => {
+    try {
+      let response = await fetch(
+        `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+      );
+      let jsonRespose = await response.json();
+      let result = {
+        city: city,
+        temp: jsonRespose.main.temp,
+        tempMin: jsonRespose.main.temp_min,
+        tempMax: jsonRespose.main.temp_max,
+        humidity: jsonRespose.main.humidity,
+        feelslike: jsonRespose.main.feels_like,
+        weather: jsonRespose.weather[0].description,
+      };
+      console.log(result);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  let handleSubmit = async (evt) => {
+    try {
+      evt.preventDefault();
+      console.log(city);
+      setCity("");
+      let newInfo = await getWeatherInfo();
+      updateInfo(newInfo);
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className="SearchBox">
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
+        <br></br>
+        <br></br>
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+        {error && (
+          <p style={{ color: "red" }}>
+            Weather information about this place is not avaialabe.
+          </p>
+        )}
+      </form>
+    </div>
+  );
+}
